002ccf33718c52bfc86db150a6bb0cb64458b536
jconklinsc <jconklinsc@gmail.com>
2025-12-12 09:41:41 -0700

    Update layout.js

--- packages/next-export-example/components/layout.js
+++ packages/next-export-example/components/layout.js
@@ -6,10 +6,13 @@ export default function Layout({ children }) {
     <div
       style={{
         padding: '24px',
-        fontFamily: 'system-ui',
+        fontFamily:
+          'system-ui, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica, Arial, sans-serif',
         maxWidth: '700px',
         margin: '0 auto',
-        lineHeight: '1.6',
+        lineHeight: '1.75',
+        fontSize: '17px',
+        color: '#222',
         position: 'relative'
       }}
     >
@@ -19,8 +22,10 @@ export default function Layout({ children }) {
       {/* Page Header */}
       <Header />
 
-      {/* Page Content */}
-      {children}
+      {/* Page Content Wrapper */}
+      <div style={{ marginTop: '24px' }}>
+        {children}
+      </div>
     </div>
   );
 }
