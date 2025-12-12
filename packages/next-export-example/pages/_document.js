66442de4f42ea66687cff3b8ce8d1d2e130764b7
jconklinsc <jconklinsc@gmail.com>
2025-12-12 14:58:01 -0700

    Update _document.js

--- packages/next-export-example/pages/_document.js
+++ packages/next-export-example/pages/_document.js
@@ -7,7 +7,23 @@ class MyDocument extends Document {
         <Head>
           <link rel="manifest" href="/manifest.json" />
           <meta name="theme-color" content="#0f1111" />
+
+          {/* Register Service Worker for Offline Support */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                if ('serviceWorker' in navigator) {
+                  window.addEventListener('load', () => {
+                    navigator.serviceWorker
+                      .register('/sw.js')
+                      .catch(err => console.log('SW registration failed:', err));
+                  });
+                }
+              `
+            }}
+          />
         </Head>
+
         <body>
           <Main />
           <NextScript />
