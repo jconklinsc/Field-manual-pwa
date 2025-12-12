e1dd4c3ab518b49bf10b82dc7a92b1e41b7060a8
jconklinsc <jconklinsc@gmail.com>
2025-12-12 09:30:22 -0700

    Create Header.js

---
+++ packages/next-export-example/components/Header.js
@@ -0,0 +1,18 @@
+export default function Header() {
+  return (
+    <header style={{
+      paddingBottom: '12px',
+      marginBottom: '24px',
+      borderBottom: '1px solid #ddd',
+    }}>
+      <h2 style={{
+        margin: 0,
+        fontSize: '20px',
+        fontWeight: '600',
+        letterSpacing: '0.5px'
+      }}>
+        Draw It Out® Field Manual
+      </h2>
+    </header>
+  );
+}
