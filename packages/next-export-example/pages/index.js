fb7df0c454bba805acb4ad30a7bd3120837a38f4
jconklinsc <jconklinsc@gmail.com>
2025-12-12 09:13:50 -0700

    Update index.js

--- packages/next-export-example/pages/index.js
+++ packages/next-export-example/pages/index.js
@@ -1,8 +1,17 @@
-<ul>
-  <li><a href="/daily-care">Daily Care</a></li>
-  <li><a href="/pre-ride">Pre-Ride Prep</a></li>
-  <li><a href="/post-ride">Post-Ride Recovery</a></li>
-  <li><a href="/skin-and-legs">Skin & Legs</a></li>
-  <li><a href="/environment">Environment & Stress</a></li>
-  <li><a href="/quick-reference">Quick Reference</a></li>
-</ul>
+export default function Home() {
+  return (
+    <main style={{ padding: '24px', fontFamily: 'system-ui' }}>
+      <h1>Field Manual</h1>
+      <p>Practical care. Clear decisions.</p>
+
+      <ul>
+        <li><a href="/daily-care">Daily Care</a></li>
+        <li><a href="/pre-ride">Pre-Ride Prep</a></li>
+        <li><a href="/post-ride">Post-Ride Recovery</a></li>
+        <li><a href="/skin-and-legs">Skin & Legs</a></li>
+        <li><a href="/environment">Environment & Stress</a></li>
+        <li><a href="/quick-reference">Quick Reference</a></li>
+      </ul>
+    </main>
+  );
+}
