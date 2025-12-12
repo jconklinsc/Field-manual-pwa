50d090e12324806194605fe2e1bea70176b82c5c
jconklinsc <jconklinsc@gmail.com>
2025-12-12 09:18:30 -0700

    Update skin-and-legs.js

--- packages/next-export-example/pages/skin-and-legs.js
+++ packages/next-export-example/pages/skin-and-legs.js
@@ -1,14 +1,15 @@
 import Nav from '../components/Nav';
+import Layout from '../components/Layout';
 
 export default function SkinAndLegs() {
   return (
-    <main style={{ padding: '24px', fontFamily: 'system-ui' }}>
+    <Layout>
       <Nav />
       <h1>Skin & Legs</h1>
       <p>
         Legs and skin tell the truth. This section teaches quick checks and early indicators
         before issues become problems.
       </p>
-    </main>
+    </Layout>
   );
 }
