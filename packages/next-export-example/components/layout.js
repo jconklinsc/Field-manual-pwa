7805e3129da35a45af63865af2f57e0d727a0330
jconklinsc <jconklinsc@gmail.com>
2025-12-12 09:17:18 -0700

    Create layout.js

---
+++ packages/next-export-example/components/layout.js
@@ -0,0 +1,13 @@
+export default function Layout({ children }) {
+  return (
+    <div style={{
+      padding: '24px',
+      fontFamily: 'system-ui',
+      maxWidth: '700px',
+      margin: '0 auto',
+      lineHeight: '1.6'
+    }}>
+      {children}
+    </div>
+  );
+}
