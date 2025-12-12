8e4736924d10c4f49e160a34ad146ba6d8d17eb1
jconklinsc <jconklinsc@gmail.com>
2025-12-12 10:00:00 -0700

    Update index.js

--- packages/next-export-example/pages/index.js
+++ packages/next-export-example/pages/index.js
@@ -1,19 +1,34 @@
 import Layout from '../components/Layout';
+import Search from '../components/Search';
+import searchData from '../data/searchData';
 
 export default function Home() {
   return (
     <Layout>
+
+      {/* Global Search */}
+      <Search data={searchData} />
+
       <h1>Field Manual</h1>
       <p>Practical care. Clear decisions.</p>
 
-      <ul>
-        <li><a href="/daily-care">Daily Care</a></li>
-        <li><a href="/pre-ride">Pre-Ride Prep</a></li>
-        <li><a href="/post-ride">Post-Ride Recovery</a></li>
-        <li><a href="/skin-and-legs">Skin & Legs</a></li>
-        <li><a href="/environment">Environment & Stress</a></li>
-        <li><a href="/quick-reference">Quick Reference</a></li>
+      <ul
+        style={{
+          listStyle: 'none',
+          padding: 0,
+          marginTop: '24px',
+          fontSize: '18px',
+          lineHeight: '1.8'
+        }}
+      >
+        <li><a href="/daily-care" style={{ textDecoration: 'underline' }}>Daily Care</a></li>
+        <li><a href="/pre-ride" style={{ textDecoration: 'underline' }}>Pre-Ride Prep</a></li>
+        <li><a href="/post-ride" style={{ textDecoration: 'underline' }}>Post-Ride Recovery</a></li>
+        <li><a href="/skin-and-legs" style={{ textDecoration: 'underline' }}>Skin & Legs</a></li>
+        <li><a href="/environment" style={{ textDecoration: 'underline' }}>Environment & Stress</a></li>
+        <li><a href="/quick-reference" style={{ textDecoration: 'underline' }}>Quick Reference</a></li>
       </ul>
+
     </Layout>
   );
 }
