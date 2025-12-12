50067def06472b00e5fdf771556a9c8ec172c640
jconklinsc <jconklinsc@gmail.com>
2025-12-12 09:08:25 -0700

    Add Nav component

--- packages/next-export-example/components/Nav.js
+++ packages/next-export-example/components/Nav.js
@@ -1,1 +1,14 @@
+export default function Nav() {
+  return (
+    <nav style={{
+      marginBottom: '24px',
+      fontSize: '16px',
+      display: 'flex',
+      gap: '16px'
+    }}>
+      <a href="/" style={{ textDecoration: 'underline' }}>Home</a>
+      <a href="javascript:history.back()" style={{ textDecoration: 'underline' }}>Back</a>
+    </nav>
+  );
+}
 
