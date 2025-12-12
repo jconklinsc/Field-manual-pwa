22662b784134f3c0a659ba1a9d4493f8787ac3de
jconklinsc <jconklinsc@gmail.com>
2025-12-12 09:33:11 -0700

    Update layout.js

--- packages/next-export-example/components/layout.js
+++ packages/next-export-example/components/layout.js
@@ -1,13 +1,25 @@
+import Drawer from './Drawer';
+import Header from './Header';
+
 export default function Layout({ children }) {
   return (
-    <div style={{
-      padding: '24px',
-      fontFamily: 'system-ui',
-      maxWidth: '700px',
-      margin: '0 auto',
-      lineHeight: '1.6'
-    }}>
+    <div
+      style={{
+        padding: '24px',
+        fontFamily: 'system-ui',
+        maxWidth: '700px',
+        margin: '0 auto',
+        lineHeight: '1.6',
+        position: 'relative'
+      }}
+    >
+      {/* Slide-out Navigation Drawer */}
+      <Drawer />
+
+      {/* Page Header */}
       <Header />
+
+      {/* Page Content */}
       {children}
     </div>
   );
