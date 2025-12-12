c95b93152ae17398168932fb745d188d992cb169
jconklinsc <jconklinsc@gmail.com>
2025-12-12 14:40:49 -0700

    Create Layout.js

---
+++ packages/next-export-example/components/Layout.js
@@ -0,0 +1,21 @@
+import Drawer from './Drawer';
+import Header from './Header';
+
+export default function Layout({ children }) {
+  return (
+    <div
+      style={{
+        padding: '24px',
+        fontFamily: 'system-ui',
+        maxWidth: '700px',
+        margin: '0 auto',
+        lineHeight: '1.6',
+        position: 'relative'
+      }}
+    >
+      <Drawer />
+      <Header />
+      {children}
+    </div>
+  );
+}
