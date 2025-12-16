699dc0fa6f8c9d632c29aeb4a45e7a5f8a311612
jconklinsc <jconklinsc@gmail.com>
2025-12-16 14:44:06 -0700

    Update next.config.js

--- packages/next-export-example/next.config.js
+++ packages/next-export-example/next.config.js
@@ -1,11 +1,14 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   async redirects() {
     return [
       {
         source: '/enviroment',
         destination: '/environment',
-        permanent: true,
+        permanent: true, // 301 redirect
       },
     ];
   },
 };
+
+module.exports = nextConfig;
