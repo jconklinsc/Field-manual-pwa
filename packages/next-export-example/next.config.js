72f8a6505489a0657252cbfac74452064c1125f9
jconklinsc <jconklinsc@gmail.com>
2025-12-16 14:25:41 -0700

    Update next.config.js

--- packages/next-export-example/next.config.js
+++ packages/next-export-example/next.config.js
@@ -1,21 +1,11 @@
-/** @type {import('next').NextConfig} */
-
-// Next.js 9 compatibility settings + PWA allowance
-const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/enviroment',
+        destination: '/environment',
+        permanent: true,
+      },
+    ];
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-
-  // Disable Minifier for older Next.js builds to prevent SWC + OpenSSL issues
-  experimental: {
-    swcMinify: false
-  },
-
-  // Required for stable PWA behavior with a custom service worker
-  poweredByHeader: false,
 };
-
-module.exports = nextConfig;
