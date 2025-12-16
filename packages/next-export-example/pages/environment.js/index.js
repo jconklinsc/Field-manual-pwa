a3f115d6af4d1d58edf703188ab98213804f8e06
jconklinsc <jconklinsc@gmail.com>
2025-12-16 14:51:13 -0700

    Create index.js

---
+++ packages/next-export-example/pages/environment.js/index.js
@@ -0,0 +1,86 @@
+14:44:46.562 Running build in Washington, D.C., USA (East) – iad1 (Turbo Build Machine)
+14:44:46.563 Build machine configuration: 30 cores, 60 GB
+14:44:46.571 Cloning github.com/jconklinsc/Field-manual-pwa (Branch: master, Commit: c95b931)
+14:44:46.572 Skipping build cache, deployment was triggered without cache.
+14:44:46.785 Cloning completed: 214.000ms
+14:44:47.780 Running "vercel build"
+14:44:48.145 Vercel CLI 50.0.1
+14:44:48.395 Installing dependencies...
+14:45:02.298 npm warn deprecated copy-concurrently@1.0.5: This package is no longer supported.
+14:45:02.305 npm warn deprecated uuid@3.4.0: Please upgrade  to version 7 or higher.  Older versions may use Math.random() in certain circumstances, which is known to be problematic.  See https://v8.dev/blog/math-random for details.
+14:45:02.307 npm warn deprecated fs-write-stream-atomic@1.0.10: This package is no longer supported.
+14:45:02.325 npm warn deprecated figgy-pudding@3.5.2: This module is no longer supported.
+14:45:02.491 npm warn deprecated move-concurrently@1.0.1: This package is no longer supported.
+14:45:02.691 npm warn deprecated resolve-url@0.2.1: https://github.com/lydell/resolve-url#deprecated
+14:45:02.738 npm warn deprecated source-map-url@0.4.1: See https://github.com/lydell/source-map-url#deprecated
+14:45:02.992 npm warn deprecated flatten@1.0.3: flatten is deprecated in favor of utility frameworks such as lodash.
+14:45:03.250 npm warn deprecated querystring@0.2.1: The querystring API is considered Legacy. new code should use the URLSearchParams API instead.
+14:45:03.404 npm warn deprecated glob@7.2.3: Glob versions prior to v9 are no longer supported
+14:45:03.420 npm warn deprecated inflight@1.0.6: This module is not supported, and leaks memory. Do not use it. Check out lru-cache if you want a good and tested way to coalesce async requests by a key value, which is much more comprehensive and powerful.
+14:45:03.481 npm warn deprecated rimraf@2.7.1: Rimraf versions prior to v4 are no longer supported
+14:45:03.518 npm warn deprecated urix@0.1.0: Please see https://github.com/lydell/urix#deprecated
+14:45:03.554 npm warn deprecated source-map-resolve@0.5.3: See https://github.com/lydell/source-map-resolve#deprecated
+14:45:03.848 npm warn deprecated @babel/plugin-proposal-dynamic-import@7.18.6: This proposal has been merged to the ECMAScript standard and thus this plugin is no longer maintained. Please use @babel/plugin-transform-dynamic-import instead.
+14:45:03.864 npm warn deprecated @babel/plugin-proposal-json-strings@7.18.6: This proposal has been merged to the ECMAScript standard and thus this plugin is no longer maintained. Please use @babel/plugin-transform-json-strings instead.
+14:45:03.870 npm warn deprecated @babel/plugin-proposal-optional-catch-binding@7.18.6: This proposal has been merged to the ECMAScript standard and thus this plugin is no longer maintained. Please use @babel/plugin-transform-optional-catch-binding instead.
+14:45:03.870 npm warn deprecated @babel/plugin-proposal-unicode-property-regex@7.18.6: This proposal has been merged to the ECMAScript standard and thus this plugin is no longer maintained. Please use @babel/plugin-transform-unicode-property-regex instead.
+14:45:03.878 npm warn deprecated @babel/plugin-proposal-async-generator-functions@7.20.7: This proposal has been merged to the ECMAScript standard and thus this plugin is no longer maintained. Please use @babel/plugin-transform-async-generator-functions instead.
+14:45:04.207 npm warn deprecated amphtml-validator@1.0.30: Version no longer supported. Upgrade to 1.0.35.
+14:45:04.267 npm warn deprecated @babel/plugin-proposal-numeric-separator@7.8.3: This proposal has been merged to the ECMAScript standard and thus this plugin is no longer maintained. Please use @babel/plugin-transform-numeric-separator instead.
+14:45:04.299 npm warn deprecated @babel/plugin-proposal-class-properties@7.7.0: This proposal has been merged to the ECMAScript standard and thus this plugin is no longer maintained. Please use @babel/plugin-transform-class-properties instead.
+14:45:04.304 npm warn deprecated @babel/plugin-proposal-nullish-coalescing-operator@7.7.4: This proposal has been merged to the ECMAScript standard and thus this plugin is no longer maintained. Please use @babel/plugin-transform-nullish-coalescing-operator instead.
+14:45:04.305 npm warn deprecated @babel/plugin-proposal-object-rest-spread@7.6.2: This proposal has been merged to the ECMAScript standard and thus this plugin is no longer maintained. Please use @babel/plugin-transform-object-rest-spread instead.
+14:45:04.312 npm warn deprecated @babel/plugin-proposal-optional-chaining@7.7.4: This proposal has been merged to the ECMAScript standard and thus this plugin is no longer maintained. Please use @babel/plugin-transform-optional-chaining instead.
+14:45:04.864 npm warn deprecated querystring@0.2.0: The querystring API is considered Legacy. new code should use the URLSearchParams API instead.
+14:45:05.585 
+14:45:05.585 added 941 packages in 17s
+14:45:05.585 
+14:45:05.585 52 packages are looking for funding
+14:45:05.585   run `npm fund` for details
+14:45:05.604 Detected Next.js version: 9.3.2
+14:45:05.619 Running "npm run build"
+14:45:05.743 
+14:45:05.743 > field-manual-app@1.0.0 build
+14:45:05.743 > next build
+14:45:05.743 
+14:45:06.195 Warning: No build cache found. Please configure build caching for faster rebuilds. Read more: https://err.sh/next.js/no-cache
+14:45:06.195 
+14:45:06.195 Creating an optimized production build...
+14:45:06.204 Attention: Next.js now collects completely anonymous telemetry regarding usage.
+14:45:06.204 This information is used to shape Next.js' roadmap and prioritize features.
+14:45:06.204 You can learn more, including how to opt-out if you'd not like to participate in this anonymous program, by visiting the following URL:
+14:45:06.204 https://nextjs.org/telemetry
+14:45:06.204 
+14:45:09.227 
+14:45:09.229 Compiled successfully.
+14:45:09.229 
+14:45:09.229 Automatically optimizing pages...
+14:45:09.540 
+14:45:09.560 Page                                                           Size     First Load
+14:45:09.561 ┌ ○ /                                                          2.45 kB     61.8 kB
+14:45:09.561 ├ ○ /404                                                       3.03 kB     62.4 kB
+14:45:09.561 ├ ○ /daily-care                                                2.06 kB     61.5 kB
+14:45:09.561 ├ ○ /enviroment                                                2.38 kB     61.8 kB
+14:45:09.561 ├ ○ /post-ride                                                 1.78 kB     61.2 kB
+14:45:09.561 ├ ○ /pre-ride                                                  2.02 kB     61.4 kB
+14:45:09.561 ├ ○ /quick-reference                                           2.06 kB     61.5 kB
+14:45:09.561 └ ○ /skin-and-legs                                             2.48 kB     61.9 kB
+14:45:09.561 + shared by all                                                59.4 kB
+14:45:09.561   ├ static/pages/_app.js                                       2.04 kB
+14:45:09.561   ├ chunks/ee270148c8c0facf065fc5dbd31ceee8b539d115.a76080.js  9.68 kB
+14:45:09.561   ├ chunks/framework.e18fe3.js                                 40 kB
+14:45:09.561   ├ runtime/main.af8b2f.js                                     6.95 kB
+14:45:09.561   └ runtime/webpack.b65cab.js                                  748 B
+14:45:09.561 
+14:45:09.561 λ  (Lambda)  server-side renders at runtime (uses getInitialProps or getServerSideProps)
+14:45:09.561 ○  (Static)  automatically rendered as static HTML (uses no initial props)
+14:45:09.561 ●  (SSG)     automatically generated as static HTML + JSON (uses getStaticProps)
+14:45:09.561 
+14:45:09.699 Collected static files (public/, static/, .next/static): 2.363ms
+14:45:09.729 Build Completed in /vercel/output [21s]
+14:45:09.822 Deploying outputs...
+14:45:12.045 Deployment completed
+14:45:12.809 Creating build cache...
+14:45:15.476 Created build cache: 2.666s
+14:45:15.476 Uploading build cache [14.74 MB]
+14:45:15.796 Build cache uploaded: 319.131ms
