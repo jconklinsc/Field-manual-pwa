751c33070ada001da072faa324f2ce36f785bb57
jconklinsc <jconklinsc@gmail.com>
2025-12-17 11:48:51 -0700

    Create _app.js

---
+++ packages/next-export-example/pages/_app.js
@@ -0,0 +1,33 @@
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+
+export default function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  const [isTransitioning, setIsTransitioning] = useState(false);
+
+  useEffect(() => {
+    const handleStart = () => setIsTransitioning(true);
+    const handleDone = () => setIsTransitioning(false);
+
+    router.events.on('routeChangeStart', handleStart);
+    router.events.on('routeChangeComplete', handleDone);
+    router.events.on('routeChangeError', handleDone);
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart);
+      router.events.off('routeChangeComplete', handleDone);
+      router.events.off('routeChangeError', handleDone);
+    };
+  }, [router]);
+
+  return (
+    <div
+      style={{
+        opacity: isTransitioning ? 0 : 1,
+        transition: 'opacity 200ms ease',
+      }}
+    >
+      <Component {...pageProps} />
+    </div>
+  );
+}
