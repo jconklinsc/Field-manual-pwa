64675c808be707a1b2570839618cb078fa7d0429
jconklinsc <jconklinsc@gmail.com>
2025-12-17 08:21:19 -0700

    Update Drawer.js

--- packages/next-export-example/components/Drawer.js
+++ packages/next-export-example/components/Drawer.js
@@ -2,42 +2,30 @@ import Link from 'next/link';
 
 export default function Drawer() {
   return (
-    <nav
+    <div
       style={{
         position: 'fixed',
         bottom: 0,
         left: 0,
         right: 0,
-        background: '#0f1111',
+        background: '#111',
         borderTop: '1px solid #222',
-        padding: '10px 12px',
         display: 'flex',
         justifyContent: 'space-around',
-        zIndex: 1000
+        padding: '10px 0',
+        zIndex: 1000,
       }}
     >
-      <Link href="/">
-        <a style={linkStyle}>Home</a>
-      </Link>
-
-      <Link href="/daily-care">
-        <a style={linkStyle}>Daily</a>
-      </Link>
-
-      <Link href="/notes">
-        <a style={linkStyle}>Notes</a>
-      </Link>
-
-      <Link href="/favorites">
-        <a style={linkStyle}>Saved</a>
-      </Link>
-    </nav>
+      <Link href="/"><a style={link}>Home</a></Link>
+      <Link href="/daily-care"><a style={link}>Daily</a></Link>
+      <Link href="/environment"><a style={link}>Environment</a></Link>
+      <Link href="/notes"><a style={link}>Notes</a></Link>
+    </div>
   );
 }
 
-const linkStyle = {
+const link = {
   color: '#fff',
   textDecoration: 'none',
   fontSize: '14px',
-  fontWeight: 500
 };
