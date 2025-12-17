d381d9bd668fa56c185136b8d3a8e7cb084d942a
jconklinsc <jconklinsc@gmail.com>
2025-12-17 12:04:06 -0700

    Update index.js

--- packages/next-export-example/pages/index.js
+++ packages/next-export-example/pages/index.js
@@ -1,23 +1,19 @@
 import Layout from '../components/Layout';
-import Search from '../components/Search';
-import searchData from '../data/searchData';
-import { styles } from '../components/styles';
 
 export default function Home() {
   return (
     <Layout>
-      <Search data={searchData} />
+      <h1>Field Manual</h1>
+      <p>Practical care. Clear decisions.</p>
 
-      <h1 style={styles.h1}>Field Manual</h1>
-      <p style={styles.paragraph}>Practical care. Clear decisions.</p>
-
-      <ul style={styles.list}>
+      <ul style={{ lineHeight: '1.8' }}>
         <li><a href="/daily-care">Daily Care</a></li>
         <li><a href="/pre-ride">Pre-Ride Prep</a></li>
         <li><a href="/post-ride">Post-Ride Recovery</a></li>
         <li><a href="/skin-and-legs">Skin & Legs</a></li>
         <li><a href="/environment">Environment & Stress</a></li>
         <li><a href="/quick-reference">Quick Reference</a></li>
+        <li><a href="/notes">Notes</a></li>
       </ul>
     </Layout>
   );
